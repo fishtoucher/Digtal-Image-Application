d8d4cb7f28cae9ebe79b26a1bb13cfd78b36f733
fishtoucher <853375217@qq.com>
2021-03-24 17:57:34 +0800

    v0.1.0

---
+++ Example/inpainting-master/inpainting/inpainting.cpp
@@ -0,0 +1,13 @@
+//
+//  inpainting.cpp
+//  TODO: brief descripton here
+//
+//  Created by Sooham Rafiz on 2016-05-16.
+//  Copyright © 2016 Sooham Rafiz. All rights reserved.
+
+
+// TODO: Read up on Compiler Return Value Optimization
+// you don't need to return pointers to internal data
+// TODO: also read on this:  https://www.wikiwand.com/en/Resource_Acquisition_Is_Initialization
+// TODO: openGL / GPU support on android
+
