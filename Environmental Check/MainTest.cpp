0511f1ad86bdca013e80fe090d6bcc682e042606
fishtoucher <853375217@qq.com>
2021-06-06 15:54:26 +0800

    文件整理

--- Environmental Check/MainTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "function.h"
-#include<iostream>
-
-//int main()
-//{
-//	printf("test");
-//	__test_opencv();
-//	return 0;
-//}
--- Environmental Check/function.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef FUNCATION_H
-#define FUNCATION_H
-
-extern int __test_opencv();
-extern int __test_compiler();
-
-#endif
